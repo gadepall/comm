68f316684b12d37c031bc4cc1175c17fe384b7b7
gadepall <gadepall@gmail.com>
2018-07-30 10:42:16 +0530

    first commit

---
+++ modulation/chapter1/prob1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+#This program generates a Gaussian random no with 0 mean and unit variance
+
+#Importing numy, scipy, mpmath and pyplot
+import numpy as np
+import mpmath as mp
+import scipy 
+import scipy.stats as sp
+import matplotlib.pyplot as plt
+import subprocess
+
+print np.random.normal(0,1)
