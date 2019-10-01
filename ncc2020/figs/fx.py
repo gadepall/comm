452502e1903f25ad0fb5086efc303c77d6beb749
gadepall <gadepall@gmail.com>
2019-10-01 15:07:04 +0530

    Add existing files

---
+++ ncc2020/figs/fx.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 16 20:44:29 2019
+
+@author: theresh
+"""
+
+import numpy as np
+import math as mp
+from matplotlib import pyplot as plt
+a=np.linspace(-4,4,100)
+y=np.zeros(len(a),dtype=float)
+for i in range(len(a)):
+    y[i]=- np.log(mp.tanh(a[i]))
+    
+plt.plot(a,y) 
+plt.plot(a,a)
+plt.grid(True) 
+plt.xlim(0,4)
+plt.ylim(0,4)
+plt.xlabel('$x$')
+plt.ylabel('$f(x)$')  
+plt.legend(['$f(x)=-\log (tanh (\\frac{x}{2}))$'],loc='best')
+plt.savefig('./fxgraph.eps')
